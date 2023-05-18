5cffd240d1a5d90b300be26c1aabc6f63aee50c3
HLeiTR <sy.hung@mail.utoronto.ca>
2023-05-18 22:41:49 +0800

    [+] Updated May 18

---
+++ Sect3/practice2/ExerciseComponent.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// Todo: Add your component code
+// Important: Use export default function MyCmp() { ... }, don't define "export default" on a separate line!
+export default function MyCmp() {
+    return <p>First exercise - done!</p>
+}
