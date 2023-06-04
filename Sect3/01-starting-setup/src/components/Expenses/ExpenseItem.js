b813d57c73f7eb7984629f0972a6e7789b302124
HLeiTR <sy.hung@mail.utoronto.ca>
2023-06-04 21:25:46 +0800

    [+] Updated June 4

--- Sect3/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ Sect3/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,8 @@
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   const date = props.date;
   const title = props.title;
   const amount = props.amount;
